fab0abbbd0a666968b6fb706fd57b80686c673a1
Ankur Oberoi <aoberoi@gmail.com>
2024-12-06 04:10:10 +0000

    Initializes project
    Used `swift package init`, customized project and target names, added
    swift-argument-parser dependency.
    
    Reproduces a potential bug!
    Using the `swift run` command (or `swift run CLI`) will build the
    executable target, but it will not actually run the executable. The
    expected output is "Hello Swift", but instead the command just hangs.

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AOC2024",
+    // products: [
+    //     .executable(name: "aoc2024", targets: ["CLI"]),
+    // ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
+    ],
+    targets: [
+        // .target(
+        //     name: "CLI",
+        //     dependencies: [
+        //         .product(name: "ArgumentParser", package: "swift-argument-parser")
+        //     ]
+        // )
+        .executableTarget(
+            name: "CLI",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ]
+        )
+    ]
+)
