250d62a49a5fa30eeb36a9fb03638309c0cce50e
Ankur Oberoi <aoberoi@gmail.com>
2024-12-06 06:02:35 +0000

    Renames executable product name: CLI -> aoc2024

--- Package.swift
+++ Package.swift
@@ -5,19 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "AOC2024",
-    // products: [
-    //     .executable(name: "aoc2024", targets: ["CLI"]),
-    // ],
+    products: [
+        .executable(name: "aoc2024", targets: ["CLI"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
     ],
     targets: [
-        // .target(
-        //     name: "CLI",
-        //     dependencies: [
-        //         .product(name: "ArgumentParser", package: "swift-argument-parser")
-        //     ]
-        // )
         .executableTarget(
             name: "CLI",
             dependencies: [
